24d4a3932a59eb749689e4c86faa728a47f912f2
Aidan Tran <aidant@udel.edu>
2023-04-25 02:32:20 -0400

    completed exercise 3 and notes

--- 3/prodcons.c
+++ 3/prodcons.c
@@ -14,36 +14,50 @@ int in = 0;
 int out = 0;
 int buffer[BUFFER_SIZE];
 pthread_mutex_t mutex;
-typedef struct arg_data {
-    int thread_number;  
+typedef struct arg_data
+{
+    int thread_number;
 } arg_data;
-
-void* producer(void* arg)
+void *producer(void *arg)
 {
     int item;
-    arg_data* current_thread_data = (arg_data*)arg;
-    for(int i = 0; i < MAX_ITEMS_TO_PRODUCE; i++) {
+    arg_data *current_thread_data = (arg_data *)arg;
+    for (int i = 0; i < MAX_ITEMS_TO_PRODUCE; i++)
+    {
         item = rand();
-        //start of critical section
+        // start of critical section
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = item;
         printf("Producer %d: Inserted item %d into index %d\n", current_thread_data->thread_number, item,
-        in);
-        //end of critical section
+               in);
+        in = (in + 1) % 5;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+        // end of critical section
     }
     return NULL;
 }
 
-void* consumer(void* arg)
+void *consumer(void *arg)
 {
-    arg_data* current_thread_data = (arg_data*)arg;
-    for(int i = 0; i < MAX_ITEMS_TO_CONSUME; i++) {
-        //start of critical section
-        int item;
-        item = rand();
-        printf("Consumer %d: Removed item %d from index %d\n", current_thread_data->thread_number, item, out);
-        //end of critical section
-        }
+    arg_data *current_thread_data = (arg_data *)arg;
+    for (int i = 0; i < MAX_ITEMS_TO_CONSUME; i++)
+    {
+        // start of critical section
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item = buffer[out];
+        printf("Consumer %d: Removed item %d from index %d\n", current_thread_data->thread_number,
+               item, out);
+        out = (out + 1) % 5;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+        // end of critical section
+    }
     return NULL;
 }
+
 int main()
 {
     pthread_t producers[NumProducer], consumers[NumConsumer];
@@ -52,18 +66,22 @@ int main()
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&full, 0, 0);
-    for (int thread_no = 1; thread_no <= NumProducer; thread_no++) {
+    for (int thread_no = 1; thread_no <= NumProducer; thread_no++)
+    {
         producer_arg_arr[thread_no - 1].thread_number = thread_no;
         pthread_create(&producers[thread_no - 1], NULL, producer, &producer_arg_arr[thread_no - 1]);
     }
-    for (int thread_no = 1; thread_no <= NumConsumer; thread_no++) {
+    for (int thread_no = 1; thread_no <= NumConsumer; thread_no++)
+    {
         consumer_arg_arr[thread_no - 1].thread_number = thread_no;
         pthread_create(&consumers[thread_no - 1], NULL, consumer, &consumer_arg_arr[thread_no - 1]);
     }
-    for(int i = 0; i < NumProducer; i++) {
+    for (int i = 0; i < NumProducer; i++)
+    {
         pthread_join(producers[i], NULL);
     }
-    for(int i = 0; i < NumConsumer; i++) {
+    for (int i = 0; i < NumConsumer; i++)
+    {
         pthread_join(consumers[i], NULL);
     }
     pthread_mutex_destroy(&mutex);
